e269e840fc3b1c7698dd77e2e0d37210f499555b
thalisson-medeiros <med.t.avila@gmail.com>
2022-03-14 22:16:39 -0300

    area-quadrado

---
+++ área_quadrado.py
@@ -0,0 +1,5 @@
+#área de um quadrado e o dobro
+quadrado = int(input('Digite a largura de um quadrado: '))
+área = quadrado ** 2
+dobro =  área * 2
+print(f'A área deste quadrado é de {área}m e o dobro desta área é de {dobro}m. ')
